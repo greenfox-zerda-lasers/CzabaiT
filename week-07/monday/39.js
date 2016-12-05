fe78db0e992d1620c4c1303e6f86b20b07914d67
CzabaiT <czabait@gmail.com>
2016-12-05 17:13:56 +0100

    tasks till 41

--- week-07/monday/39.js
+++ week-07/monday/39.js
@@ -3,3 +3,10 @@
 var ai = 123;
 // create a function that doubles it's input
 // double ai with it
+
+function Doubler(input) {
+  ai *= 2*input;
+  return ai;
+}
+
+console.log(Doubler(1));
--- week-07/monday/40.js
+++ week-07/monday/40.js
@@ -1,5 +1,12 @@
-'use strict' 
+'use strict'
 
 var aj = 'kuty'
 // write a function that gets a string as an input
 // and appends an 'a' character to its end and returns a new string
+
+function Appender(string) {
+  aj += 'a'
+  return aj
+}
+
+console.log(Appender(aj));
